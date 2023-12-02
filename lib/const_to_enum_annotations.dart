8f800001a18139d23bf2336ffee6b9826a538230
harshit kumar <harshit.kumar@esamudaay.com>
2023-12-02 15:47:28 +0530

    first commit

---
+++ lib/const_to_enum_annotations.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library const_to_enum_annotations;
+
+export 'src/annotations.dart';
---
+++ test/src/const_to_enum_annotations_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:const_to_enum_annotations/const_to_enum_annotations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ConstToEnumAnnotations', () {
+    test('can be instantiated', () {
+      expect(GenerateStaticConst(), isNotNull);
+    });
+  });
+}
