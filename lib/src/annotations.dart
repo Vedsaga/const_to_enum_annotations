b0ab6ad14e69e9a6f3d7328dbc743ff93bf80e37
harshit kumar <harshit.kumar@esamudaay.com>
2023-12-13 11:52:38 +0530

    adding String Value annotation

--- lib/src/annotations.dart
+++ lib/src/annotations.dart
@@ -1,10 +1,20 @@
 /// In annotations.dart
 class GenerateStaticConst {
-
   /// Creates an instance of [GenerateStaticConst].
-  /// [defaultValue] specifies the default return value for the enum conversion 
+  /// [defaultValue] specifies the default return value for the enum conversion
   /// function.
   const GenerateStaticConst({this.defaultValue});
+
   /// default which function return
   final String? defaultValue;
+}
+
+// In a separate file, e.g., string_value_annotation.dart
+
+/// Annotation to specify an alternative string value for a static const field.
+class StringValue {
+  /// constructor for the StringValue
+  const StringValue(this.value);
+  /// The alternative string value to use in the enum.
+  final String value;
 }
